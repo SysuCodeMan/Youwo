76b1dc1d092f4e9b6fa60e81119cc735add902bc
ChrisChan <569475077@qq.com>
2017-08-06 10:47:36 +0800

    学习网络访问模块化

--- app/src/main/java/com/example/davidwillo/youwo/util/NetworkUtil.java
+++ app/src/main/java/com/example/davidwillo/youwo/util/NetworkUtil.java
@@ -1,8 +1,11 @@
 package com.example.davidwillo.youwo.util;
 
+import java.net.CookieManager;
+
 /**
  * Created by ChrisChan on 2017/8/6.
  */
 
 public class NetworkUtil {
+    private CookieManager cookieManager;
 }
